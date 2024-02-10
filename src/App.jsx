3fc134153fc137164253aac28b41ab70e83f5beb
Naveen A <121672275+naveen20022002@users.noreply.github.com>
2024-02-10 22:15:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import axios from 'axios'
 import { UserContextProvider } from "./UserContext";
 
 function App() {
-axios.defaults.baseURL = "http://localhost:4000";
+// axios.defaults.baseURL = "http://localhost:4000";
+  axios.defaults.baseURL = "https://chat-application-a4ea.onrender.com";
 axios.defaults.withCredentials = true;
   return (
     <UserContextProvider> 
