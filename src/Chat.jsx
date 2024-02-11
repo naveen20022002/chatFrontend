3323ad555a9717b810dd044a442ff900bad9383a
Naveen A <121672275+naveen20022002@users.noreply.github.com>
2024-02-11 19:02:06 +0530

    Update Chat.jsx

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -107,7 +107,7 @@ const Chat = () => {
     axios.get('/people').then(res=>{
       const offlinePeopleArr = res.data
         .filter(p => p._id !== id)
-        .filter(p => !Object.keys(onlinePeople)?.includes(p._id))
+        .filter(p => !Object.keys(onlinePeople).includes(p._id))
       
         const offlinePeople = {};
         offlinePeopleArr.forEach(p=>{
@@ -119,7 +119,7 @@ const Chat = () => {
   },[onlinePeople])
   function selectedUsernameFind(){
     // console.log(offlinePeople[selectedUserId].username || onlinePeopleExclOurUser[selectedUserId].username)
-    return offlinePeople[selectedUserId]?.username || onlinePeopleExclOurUser[selectedUserId]?.username
+    return offlinePeople[selectedUserId].username || onlinePeopleExclOurUser[selectedUserId].username
   }
 
   useEffect(()=>{
@@ -168,7 +168,7 @@ const Chat = () => {
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
             <path fillRule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clipRule="evenodd" />
           </svg>
-          <div>{username? username : "hi"}</div>
+          <div>{username}</div>
           </span>
           <button onClick={logout} className='text-sm text-white bg-gray-500 py-2 px-5 rounded-md'>Logout</button>
         </div>
